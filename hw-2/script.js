d1bc69d71c12fc998896078797b25fecb9f7ebb5
Hamzat <hamzat_talhigov@mail.ru>
2023-07-02 22:56:10 +0300

    first commit

---
+++ hw-2/script.js
@@ -0,0 +1,83 @@
+/* Задание 1
+let a = 10;
+alert(a);
+alert(a + 10);
+*/
+
+/* Задание 2
+let year = 2007;
+alert(year);
+*/
+
+/* Задание 3
+let Name = 'Брендан Эйх';
+alert(Name);
+*/
+
+/* Задание 4
+let x = 10;
+let y = 2;
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
+*/
+
+/* Задание 5
+let x = 2;
+let y = 5;
+alert(x**y);
+*/
+
+/* Задание 6
+let a = 9;
+let b = 2;
+alert(a % b);
+*/
+
+/*
+let num = 1;
+num += num 5;
+num -= num  3;
+num *= num  7;
+num /= num  3;
+num += num  1;
+num -= num  1;
+alert(num);
+*/
+
+/* Задание 8
+let age = prompt('Сколько вам лет?');
+alert(age);
+*/
+
+/* Задание 9.0
+let user = {
+    name: 'Hamz',
+    age: 24,
+    isAdmin: true;
+}
+*/
+
+/* Задание 9.1
+let user.cityofresidence = 'Moscow';
+*/
+
+/* Задание 9.2
+let age = 18;
+*/
+
+/* Задание 9.3
+delete User.cityofresidence;
+*/
+
+/* Задание 9.4
+let info = promt('Какую информацию хотите узнать о пользователе?');
+console.log(user{info});
+alert(user);
+*/
+
+/*
+let name = prompt("Как вас зовут?");
+alert(`Привет, ${name}`);
+*/
